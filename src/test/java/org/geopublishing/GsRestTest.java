108e81d6f603bc3f515fb6e5b174266db13259fc
skgoogle <skgoogle@wikisquare.de>
2010-06-11 16:53:35 +0000

    Support for createDatastore deleteDatastore (also recursively including all dependencies, except styles which are not supported yet) createDatabaseDatastore, e.g. Postgis deleteDatastore (also recursively including all dependencies, except styles which are not supported yet) getLayerNames deleteLayer getLayersUsingDatastore ... The set of functions is by far not complete yet, but it can already be used for some things.

---
+++ src/test/java/org/geopublishing/GsRestTest.java
@@ -0,0 +1,21 @@
+package org.geopublishing;
+
+import org.junit.Test;
+
+public class GsRestTest extends GsRest {
+
+	public GsRestTest() {
+		super("http://localhost:8080/geoserver");
+	}
+
+	@Test
+	public void testParseDatastoresXml() {
+//		String xml = "<dataStores>  <dataStore>    <name>keckPg</name>    <atom:link xmlns:atom=\"http://www.w3.org/2005/Atom\" rel=\"alternate\" href=\"http://localhost:8085/geoserver/rest/workspaces/testWorkspace1276269538724/datastores/keckPg.xml\" type=\"application/xml\"/>  </dataStore></dataStores><coverageStores/>";
+//		List<String> parseDatastoresXml = parseDatastoresXml(xml);
+//		
+//		assertEquals(1, parseDatastoresXml.size());
+//		assertEquals("keckPg", parseDatastoresXml.get(0));
+		
+	}
+
+}
